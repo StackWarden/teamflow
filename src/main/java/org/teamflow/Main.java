987b59c4faf23d6eedd0fec8bb02cab317b82a25
Joeri Brinks <joeri1314@gmail.com>
2025-03-27 11:41:04 +0100

    login or register method

--- src/main/java/org/teamflow/Main.java
+++ src/main/java/org/teamflow/Main.java
@@ -3,5 +3,7 @@ package org.teamflow;
 public class Main {
     public static void main(String[] args) {
         // code
+        Login login = new Login();
+        login.LoginOrRegister();
     }
 }
