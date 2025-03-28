4e9c20a395964fd484e341146d1834c73dce7f83
Diyar <diyarfranklin2000@gmail.com>
2025-03-28 15:41:37 +0100

    removed extra class

--- src/main/java/org/teamflow/Main.java
+++ src/main/java/org/teamflow/Main.java
@@ -4,13 +4,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        LoginOrRegister loginOrRegister = new LoginOrRegister();
-        System.out.println(loginOrRegister.getLoginOrRegister());
+        System.out.println(getLoginOrRegister());
     }
-}
 
-class LoginOrRegister {
-    public int getLoginOrRegister() {
+    public static int getLoginOrRegister() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Do you want to login or register an account?");
         System.out.println("1. Login");
