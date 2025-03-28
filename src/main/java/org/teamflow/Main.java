081e60f69ffd169528c03c65bd3c5ca3c1280abb
joeribrinks <joeri1314@gmail.com>
2025-03-28 15:38:36 +0100

    Remove Login.java and added the LoginOrRegister class and getLoginOrRegister method to Main.java.

--- src/main/java/org/teamflow/Main.java
+++ src/main/java/org/teamflow/Main.java
@@ -1,8 +1,32 @@
 package org.teamflow;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Login login = new Login();
-        login.LoginOrRegister();
+        LoginOrRegister loginOrRegister = new LoginOrRegister();
+        System.out.println(loginOrRegister.getLoginOrRegister());
+    }
+}
+
+class LoginOrRegister {
+    public int getLoginOrRegister() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Do you want to login or register an account?");
+        System.out.println("1. Login");
+        System.out.println("2. Sign up");
+
+        int input = scanner.nextInt();
+
+        return switch (input) {
+            case 1, 2 -> {
+                System.out.println("You chose: " + input);
+                yield input;
+            }
+            default -> {
+                getLoginOrRegister();
+                yield input;
+            }
+        };
     }
 }
