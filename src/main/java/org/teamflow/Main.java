5f24fbc64231ea4318d58b9f8952488062ba86cb
Joeri1314 <joeri1314@gmail.com>
2025-03-28 15:49:53 +0100

    Login or register option (#1)
    * login or register method
    
    * remove unused comment
    
    * Remove Login.java and added the LoginOrRegister class and getLoginOrRegister method to Main.java.
    
    * removed extra class
    
    * removed get from LoginOrRegister name
    
    ---------
    
    Co-authored-by: Diyar <diyarfranklin2000@gmail.com>

--- src/main/java/org/teamflow/Main.java
+++ src/main/java/org/teamflow/Main.java
@@ -1,7 +1,29 @@
 package org.teamflow;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // code
+        System.out.println(LoginOrRegister());
+    }
+
+    public static int LoginOrRegister() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Do you want to login or register an account?");
+        System.out.println("1. Login");
+        System.out.println("2. Sign up");
+
+        int input = scanner.nextInt();
+
+        return switch (input) {
+            case 1, 2 -> {
+                System.out.println("You chose: " + input);
+                yield input;
+            }
+            default -> {
+                LoginOrRegister();
+                yield input;
+            }
+        };
     }
 }
