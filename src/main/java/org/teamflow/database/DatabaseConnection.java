c527a8f65cc3c76c25fe438a235d2dd7478fd7ad
Diyar <diyarfranklin2000@gmail.com>
2025-03-28 14:33:26 +0100

    unused commit

--- src/main/java/org/teamflow/database/DatabaseConnection.java
+++ src/main/java/org/teamflow/database/DatabaseConnection.java
@@ -3,7 +3,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DatabaseConnection {
