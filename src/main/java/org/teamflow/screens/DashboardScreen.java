04f004e82045f3541d06f6919d4da097703816be
Sitjn64 <Stijnlammers2005@gmail.com>
2025-04-04 14:52:11 +0200

    Update DashboardScreen.java

--- src/main/java/org/teamflow/screens/DashboardScreen.java
+++ src/main/java/org/teamflow/screens/DashboardScreen.java
@@ -2,11 +2,16 @@ package org.teamflow.screens;
 
 import org.teamflow.ScreenManager;
 import org.teamflow.controllers.ProjectController;
+import org.teamflow.database.DatabaseConnection;
 import org.teamflow.enums.ScreenType;
 import org.teamflow.interfaces.Screen;
 import org.teamflow.controllers.UserController;
 import org.teamflow.models.Project;
 import org.teamflow.models.ProjectCreationResult;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class DashboardScreen implements Screen {
@@ -44,7 +49,7 @@ public class DashboardScreen implements Screen {
                     return;
                 }
                 case "1" -> createProject();
-                case "2" -> System.out.println("TODO: join project");
+                case "2" -> joinProject();
                 case "3" -> System.out.println("TODO: list projects");
                 case "4" -> {
                     userController.logout(); // You should add this method
@@ -78,6 +83,47 @@ public class DashboardScreen implements Screen {
             System.out.println("Project already exists.");
         } else {
             System.out.println("Error creating project.");
+        }
+    }
+    public void listProjects() {
+        String sql = "SELECT name FROM project";
+        try (
+                PreparedStatement stmt = DatabaseConnection.getConnection().prepareStatement(sql);
+                ResultSet rs = stmt.executeQuery()
+        ) {
+            int index = 1;
+            while (rs.next()) {
+                String name = rs.getString("name");
+                System.out.println(index + ": " + name);
+                index++;
+            }
+
+            if (index > index) {
+                System.out.println("No projects found.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Failed to list projects: " + e.getMessage());
+        }
+    }
+
+    private void joinProject() {
+        System.out.println("Which project to join?");
+        listProjects();
+        String choice = scanner.nextLine();
+        switch (choice) {
+            case "1" -> {
+                String sql = "SELECT name FROM user JOIN user_project ON user_project.id = user.id JOIN project ON project.id = project.id";
+                try (
+                        PreparedStatement stmt = DatabaseConnection.getConnection().prepareStatement(sql);
+                        ResultSet rs = stmt.executeQuery()
+                ) {
+
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+
+
+            }
         }
     }
 }
